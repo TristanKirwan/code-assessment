28f81546035acd3180a612374738577402bf4959
FHoogland <33831870+TristanKirwan@users.noreply.github.com>
2025-07-08 11:37:52 +0200

    Feat: Added collapsible footer headings

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,13 @@
 class CollapsibleFooter extends Collapsible {
   constructor() {
     super();
+
+    this.listHeadings = this.querySelectorAll('.collapsable-list button');
+
+    this.listHeadings.forEach((list) => {
+      list.addEventListener('click', (e) => this.toggleList(e));
+    });
+
     if (!this.options.breakpoint) {
       this.options.breakpoint = parseInt(768);
     }
@@ -19,17 +26,33 @@ class CollapsibleFooter extends Collapsible {
       this.displayDetails();
     });
   }
+  
+  toggleList(event) {
+    const target = event.target.closest('.collapsable-list');
+    const isOpen = target.getAttribute('data-open');
+    if(isOpen === 'true') {
+      this.close(target);
+    } else {
+      this.open(target);
+    }
+  }
 
   open(group) {
     if (window.innerWidth < this.options.breakpoint) {
       super.open(group);
     }
+    group.setAttribute('data-open', 'true');
   }
 
   close(group) {
     if (window.innerWidth < this.options.breakpoint) {
       super.close(group);
     }
+    group.setAttribute('data-open', 'false');
+  }
+
+  openAll(){
+
   }
 
   displayDetails() {
